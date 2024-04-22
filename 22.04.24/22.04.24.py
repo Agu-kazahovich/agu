33db22bcd258a29833957bb6546a5d4f8548e2b5
Agu_kazahovich <agybajbisamet@gmail.com>
2024-04-22 16:42:46 +0500

    first commit

--- 22.04.24/22.04.24.py
+++ 22.04.24/22.04.24.py
@@ -4,12 +4,12 @@ def multiple(x,y=10):
 a = multiple(4)
 print(a)
 
-def g(x,y):
-    
-    j = g(x + y)
-    if g > 100:
-        print(g,"100 ден үлкен")
+
+def check_numbers(x,y):
+    if x + y > 100:
+        return "100 ден үлкен"
     else:
-        print(g,"улкен")
-    
-    return x,y
+        return "кіші"
+
+
+print(check_numbers(50,51))
